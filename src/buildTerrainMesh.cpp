111c794f3142622ca06e29ffec5f2157c4f8bd52
SpencerClark5 <spencerc5936@gmail.com>
2022-10-17 11:47:02 -0500

    BuildTerrain Mesh

--- src/buildTerrainMesh.cpp
+++ src/buildTerrainMesh.cpp
@@ -9,7 +9,22 @@ void buildTerrainMesh(ofMesh& terrainMesh, const ofShortPixels& heightmap,
     {
         for (int y = yStart; y <= yEnd; y++)
         {
-            terrainMesh.addVertex(vec3(x * scale.x, y * scale.z, scale.y));
+            terrainMesh.addVertex(vec3(x * scale.x, scale.y * (heightmap.getColor(x,y).r), y * scale.z));
         }
     }
+    
+    for (int x = xStart; x <= xEnd -1; x++)
+    {
+        for (int y = yStart; y <= yEnd -1; y++)
+        {
+            terrainMesh.addIndex( x*(xEnd-xStart) + y);
+            terrainMesh.addIndex(x * (xEnd - xStart) + y + 1);
+            terrainMesh.addIndex((x + 1) * (xEnd - xStart) + y);
+            terrainMesh.addIndex((x + 1) * (xEnd - xStart) + y);
+            terrainMesh.addIndex(x * (xEnd - xStart) + y + 1);
+            terrainMesh.addIndex((x + 1) * (xEnd - xStart) + y + 1);
+        }
+    }
+
+    terrainMesh.flatNormals();
 }
